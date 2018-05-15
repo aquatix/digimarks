b36cd8db6b5c542915324645f1c7f10d30a46b76
Michiel Scholten <michiel@diginaut.net>
2018-05-15 11:44:53 +0200

    Also strip enter characters

--- templates/bookmarks.js
+++ templates/bookmarks.js
@@ -3,9 +3,9 @@ var instance = M.Autocomplete.getInstance(elem);
 instance.updateData({
     {% for bookmark in bookmarks %}
         {% if bookmark.favicon %}
-            "{{ bookmark.title | replace('"', '\\"') | replace('\n', '') }}": "{{ url_for('static', filename='favicons/' + bookmark.favicon) }}",
+            "{{ bookmark.title | replace('"', '\\"') | replace('\n', '') | replace('\r', '') }}": "{{ url_for('static', filename='favicons/' + bookmark.favicon) }}",
         {% else %}
-            "{{ bookmark.title | replace('"', '\\"') | replace('\n', '') }}": null,
+            "{{ bookmark.title | replace('"', '\\"') | replace('\n', '') | replace('\r', '') }}": null,
         {% endif %}
     {% endfor %}
 });
