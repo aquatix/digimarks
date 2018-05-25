f6401a3e9fed46d779e18cc2543c6535d667027f
Michiel Scholten <michiel@diginaut.net>
2018-05-25 12:11:17 +0200

    Problems found by Codacy

--- static/js/init.js
+++ static/js/init.js
@@ -1,9 +1,9 @@
-var options = {}
-var elem = document.querySelector('.sidenav');
+var options = {};
+var elem = document.querySelector(".sidenav");
 var instance = M.Sidenav.init(elem, options);
 
-var elem = document.querySelector('.collapsible');
-var instance = M.Collapsible.init(elem, {
+elem = document.querySelector(".collapsible");
+instance = M.Collapsible.init(elem, {
     // inDuration: 1000,
     // outDuration: 1000
 });
