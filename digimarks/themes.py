51b94e4b6403c99bc0c69ca394d7bf8df34bcbc2
Michiel Scholten <michiel@diginaut.net>
2018-10-15 16:22:58 +0200

    Moving around a lot of stuff

---
+++ digimarks/themes.py
@@ -0,0 +1,126 @@
+"""digimarks theme definitions"
+
+DEFAULT_THEME = 'freshgreen'
+themes = {
+    'green': {
+        'BROWSERCHROME': '#2e7d32',  # green darken-2
+        'BODY': 'grey lighten-4',
+        'TEXT': 'black-text',
+        'TEXTHEX': '#000',
+        'NAV': 'green darken-3',
+        'PAGEHEADER': 'grey-text lighten-5',
+        'MESSAGE_BACKGROUND': 'orange lighten-2',
+        'MESSAGE_TEXT': 'white-text',
+        'ERRORMESSAGE_BACKGROUND': 'red darken-1',
+        'ERRORMESSAGE_TEXT': 'white-text',
+        'BUTTON': '#1b5e20', # green darken-4
+        'BUTTON_ACTIVE': '#43a047',  # green darken-1
+        'LINK_TEXT': '#1b5e20',  # green darken-4
+        'CARD_BACKGROUND': 'green darken-3',
+        'CARD_TEXT': 'white-text',
+        'CARD_LINK': '#FFF',  # white-text
+        'CHIP_TEXT': '#1b5e20',  # green darken-4
+        'FAB': 'red',
+
+        'STAR': 'yellow-text',
+        'PROBLEM': 'red-text',
+        'COMMENT': '',
+    },
+    'freshgreen': {
+        'BROWSERCHROME': '#43a047',  # green darken-1
+        'BODY': 'grey lighten-5',
+        'TEXT': 'black-text',
+        'TEXTHEX': '#000',
+        'NAV': 'green darken-1',
+        'PAGEHEADER': 'grey-text lighten-5',
+        'MESSAGE_BACKGROUND': 'orange lighten-2',
+        'MESSAGE_TEXT': 'white-text',
+        'ERRORMESSAGE_BACKGROUND': 'red darken-1',
+        'ERRORMESSAGE_TEXT': 'white-text',
+        'BUTTON': '#1b5e20', # green darken-4
+        'BUTTON_ACTIVE': '#43a047',  # green darken-1
+        'LINK_TEXT': '#1b5e20',  # green darken-4
+        'CARD_BACKGROUND': 'green darken-1',
+        'CARD_TEXT': 'white-text',
+        'CARD_LINK': '#FFF',  # white-text
+        'CHIP_TEXT': '#1b5e20',  # green darken-4
+        'FAB': 'red',
+
+        'STAR': 'yellow-text',
+        'PROBLEM': 'red-text',
+        'COMMENT': '',
+    },
+    'lightblue': {
+        'BROWSERCHROME': '#0288d1',  # light-blue darken-2
+        'BODY': 'white',
+        'TEXT': 'black-text',
+        'TEXTHEX': '#000',
+        'NAV': 'light-blue darken-2',
+        'PAGEHEADER': 'grey-text lighten-5',
+        'MESSAGE_BACKGROUND': 'orange lighten-2',
+        'MESSAGE_TEXT': 'white-text',
+        'ERRORMESSAGE_BACKGROUND': 'red darken-1',
+        'ERRORMESSAGE_TEXT': 'white-text',
+        'BUTTON': '#fb8c00', # orange darken-1
+        'BUTTON_ACTIVE': '#ffa726',  # orange lighten-1
+        'LINK_TEXT': '#FFF',  # white
+        'CARD_BACKGROUND': 'light-blue lighten-2',
+        'CARD_TEXT': 'black-text',
+        'CARD_LINK': '#263238',  # blue-grey-text darken-4
+        'CHIP_TEXT': '#FFF',  # white
+        'FAB': 'light-blue darken-4',
+
+        'STAR': 'yellow-text',
+        'PROBLEM': 'red-text',
+        'COMMENT': '',
+    },
+    'dark': {
+        'BROWSERCHROME': '#212121',  # grey darken-4
+        'BODY': 'grey darken-4',
+        'TEXT': 'grey-text lighten-1',
+        'TEXTHEX': '#bdbdbd',
+        'NAV': 'grey darken-3',
+        'PAGEHEADER': 'grey-text lighten-1',
+        'MESSAGE_BACKGROUND': 'orange lighten-2',
+        'MESSAGE_TEXT': 'white-text',
+        'ERRORMESSAGE_BACKGROUND': 'red darken-1',
+        'ERRORMESSAGE_TEXT': 'white-text',
+        'BUTTON': '#fb8c00', # orange darken-1
+        'BUTTON_ACTIVE': '#ffa726',  # orange lighten-1
+        'LINK_TEXT': '#fb8c00',  # orange-text darken-1
+        'CARD_BACKGROUND': 'grey darken-3',
+        'CARD_TEXT': 'grey-text lighten-1',
+        'CARD_LINK': '#fb8c00',  # orange-text darken-1
+        'CHIP_TEXT': '#fb8c00',  # orange-text darken-1
+        'FAB': 'red',
+
+        'STAR': 'yellow-text',
+        'PROBLEM': 'red-text',
+        'COMMENT': '',
+    },
+    'amoled': {
+        'BROWSERCHROME': '#000',  # grey darken-4
+        'BODY': 'black',
+        'TEXT': 'grey-text lighten-1',
+        'TEXTHEX': '#bdbdbd',
+        'NAV': 'grey darken-3',
+        'PAGEHEADER': 'grey-text lighten-1',
+        'MESSAGE_BACKGROUND': 'orange lighten-2',
+        'MESSAGE_TEXT': 'white-text',
+        'ERRORMESSAGE_BACKGROUND': 'red darken-1',
+        'ERRORMESSAGE_TEXT': 'white-text',
+        'BUTTON': '#fb8c00', # orange darken-1
+        'BUTTON_ACTIVE': '#ffa726',  # orange lighten-1
+        'LINK_TEXT': '#fb8c00',  # orange-text darken-1
+        'CARD_BACKGROUND': 'grey darken-3',
+        'CARD_TEXT': 'grey-text lighten-1',
+        'CARD_LINK': '#fb8c00',  # orange-text darken-1
+        'CHIP_TEXT': '#fb8c00',  # orange-text darken-1
+        'FAB': 'red',
+
+        'STAR': 'yellow-text',
+        'PROBLEM': 'red-text',
+        'COMMENT': '',
+    }
+}
+
