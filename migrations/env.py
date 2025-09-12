b6a81fded46370988659130760983d0a05fb4ad2
Michiel Scholten <michiel@diginaut.net>
2025-09-12 12:15:25 +0200

    Make alembic comprehend/use sqlmodel

--- migrations/env.py
+++ migrations/env.py
@@ -7,7 +7,7 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 from sqlmodel import SQLModel
 
-# from app.models import Bookmark
+from src.digimarks.main import Bookmark, PublicTag, User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -62,10 +62,7 @@ def do_run_migrations(connection: Connection) -> None:
 
 
 async def run_async_migrations() -> None:
-    """In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
+    """In this scenario we need to create an Engine and associate a connection with the context."""
     connectable = async_engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix='sqlalchemy.',
